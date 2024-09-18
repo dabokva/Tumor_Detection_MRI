5ebda990a2cad4f8ec27372729a41b2fc6b236ac
dabokva <dabokva@gmail.com>
2024-09-18 22:06:45 +0200

    smaller augmentation

--- scripts/save_utils.py
+++ scripts/save_utils.py
@@ -1,4 +1,7 @@
+import os
 import pickle
+
+default_value = 0
 
 def save_data(file_path, *variables):
     try:
@@ -7,15 +10,19 @@ def save_data(file_path, *variables):
                 pickle.dump(variables[0], file)
             else:
                 pickle.dump(variables, file)
-        print(f"All variables have been successfully saved to {file_path}.")
+        print(f"all variables have been successfully saved to {file_path}.")
     except Exception as e:
-        print(f"An error occurred while saving data: {e}")
+        print(f"an error occurred while saving data: {e}")
 
-def load_data(file_path):
-    try:
-        with open(file_path, 'rb') as file:
-            variables = pickle.load(file)
-            return variables
-    except Exception as e:
-        print(f"An error occurred while loading data: {e}")
-        return None
+def loader(file_path):
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'rb') as file:
+                variables = pickle.load(file)
+                return variables
+        except Exception as e:
+            print(f"an error occurred while loading data: {e}")
+            return default_value
+    else:
+        print(f'file not found: {file_path}')
+        return default_value
