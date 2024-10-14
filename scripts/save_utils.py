9918c7f4c56f8c801bf434383e8322b75f53bcc2
dabokva <dabokva@gmail.com>
2024-10-14 21:43:09 +0200

    finished notebook run

--- scripts/save_utils.py
+++ scripts/save_utils.py
@@ -1,5 +1,7 @@
+import os
 import pickle
 import pandas as pd
+from keras.models import load_model
 
 def save_data(object, category, file_path):
 
@@ -11,14 +13,14 @@ def save_data(object, category, file_path):
             pickle.dump(object, file)
         with open('..\\save_files\\saved_data.pkl', 'wb') as file:
             pickle.dump(saved_data, file)
-        print(f'object successfuly saved to: {file_path}')
+        print(f'object successfully saved to: {file_path}.')
     else:
         saved_data = pd.concat([saved_data, pd.DataFrame({'path': [file_path], 'object':[object], 'category':[category]})], ignore_index=True)
         with open(file_path, 'wb') as file:
             pickle.dump(object, file)
         with open('..\\save_files\\saved_data.pkl', 'wb') as file:
             pickle.dump(saved_data, file)
-        print(f'object successfuly saved to: {file_path}')
+        print(f'object successfully saved to: {file_path}.')
 
 def load_data(file_path):
 
@@ -28,9 +30,19 @@ def load_data(file_path):
     if file_path in saved_data['path'].values:
         with open(file_path, 'rb') as file:
             result = pickle.load(file)
+            print(f'the object from {file_path} has been successfully loaded.')
             return result
     else:
-        print('error: no such file')
+        print(f"error: no such file with given path - {file_path}.")
         return None
+    
 
-
+def load_ml_model(file_path):
+    
+    if os.path.exists(file_path):
+        model = load_model(file_path)
+        print(f'the model from {file_path} has been successfully loaded.')
+        return model
+    else:
+        print(f"error: no such file with given path - {file_path}.")
+        return None
