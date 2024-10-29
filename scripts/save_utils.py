470fff03b84cc254e6396d80fb7a0db74a726454
dabokva <dabokva@gmail.com>
2024-10-29 17:01:30 +0100

    full rework

--- scripts/save_utils.py
+++ scripts/save_utils.py
@@ -1,21 +1,30 @@
 import os
 import pickle
 import pandas as pd
+import tensorflow as tf
 from keras.models import load_model
+
+# --- EXPERIMENTAL ---
+
+# options = tf.saved_model.LoadOptions(experimental_io_device='/job:localhost')
+# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # Disables GPU
+
+# --------------------
+
 
 def save_data(object, category, file_path):
 
     with open('..\\save_files\\saved_data.pkl', 'rb') as file:
         saved_data = pickle.load(file)
 
-    if file_path in saved_data['path'].values:
+    if file_path in saved_data['file_path'].values:
         with open(file_path, 'wb') as file:
             pickle.dump(object, file)
         with open('..\\save_files\\saved_data.pkl', 'wb') as file:
             pickle.dump(saved_data, file)
         print(f'object successfully saved to: {file_path}.')
     else:
-        saved_data = pd.concat([saved_data, pd.DataFrame({'path': [file_path], 'object':[object], 'category':[category]})], ignore_index=True)
+        saved_data = pd.concat([saved_data, pd.DataFrame({'object':[object], 'category':[category], 'file_path': [file_path]})], ignore_index=True)
         with open(file_path, 'wb') as file:
             pickle.dump(object, file)
         with open('..\\save_files\\saved_data.pkl', 'wb') as file:
@@ -24,10 +33,10 @@ def save_data(object, category, file_path):
 
 def load_data(file_path):
 
-    with open('..\\save_files\\saved_data.pkl', 'rb') as file:
+    with open(r'..\save_files\saved_data.pkl', 'rb') as file: # changed \\ to \ and added r
         saved_data = pickle.load(file)
 
-    if file_path in saved_data['path'].values:
+    if file_path in saved_data['file_path'].values:
         with open(file_path, 'rb') as file:
             result = pickle.load(file)
             print(f'the object from {file_path} has been successfully loaded.')
@@ -40,9 +49,10 @@ def load_data(file_path):
 def load_ml_model(file_path):
     
     if os.path.exists(file_path):
-        model = load_model(file_path)
+        loaded_model = load_model(file_path, options=options)
+        #model = load_model(file_path)
         print(f'the model from {file_path} has been successfully loaded.')
-        return model
+        return loaded_model
     else:
         print(f"error: no such file with given path - {file_path}.")
         return None
